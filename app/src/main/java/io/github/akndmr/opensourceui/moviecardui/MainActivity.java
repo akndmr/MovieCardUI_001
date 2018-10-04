ecc0a94fff9ac2b8bbe60e0de2f0b2f511a8b7f8
Akın Demir <akin.demir@ogr.sakarya.edu.tr>
2018-10-04 03:14:20 +0300

    Initial commit

---
+++ app/src/main/java/io/github/akndmr/opensourceui/moviecardui/MainActivity.java
@@ -0,0 +1,20 @@
+package io.github.akndmr.opensourceui.moviecardui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+*
+*  Created by github.com/akndmr
+*  for OpenSourceUI project
+*
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
